572ed0848a7af3925402a1a81433911a2be6b26f
vlad <vladsheverda7@gmail.com>
2025-05-06 19:09:14 +0400

    Add selectors.js, dev-users.json, urls.json, l10n.json and requirements.js files

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
