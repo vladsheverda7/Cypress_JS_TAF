a62aa1ab6ceffd9f3bfcd0d2e33166c600f5c3a7
vlad <vladsheverda7@gmail.com>
2025-05-07 12:53:40 +0400

    Add login page positive test

--- cypress/integration/ui/login-page.ui.spec.js
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -1,21 +1,24 @@
-const { loginPage } = require('../../support/selectors');
+const selectors = require('../../support/selectors');
 import l10n from '../../support/l10n.json';
+import users from '../../sensitive-data/dev-users.json';
+import '../../support/commands';
+import { pages } from '../../support/urls';
 
 describe('LoginPage: Given Login page is opened', { testIsolation: false }, () => {
-  before(() => {
-    cy.visit('/');
-  });
-
   context('LoginPage: When user reviews the page', () => {
+    before(() => {
+      cy.visit('/');
+    });
+
     it('LoginPage: Then user see empty Username field', () => {
-      cy.get(loginPage.userNameInputField)
+      cy.get(selectors.loginPage.userNameInputField)
         .should('have.attr', 'placeholder', l10n.en.loginPage.form.userNameInputField)
         .and('have.value', '')
         .and('be.visible');
     });
 
     it('LoginPage: Then user see empty Password field', () => {
-      cy.get(loginPage.passwordInputField)
+      cy.get(selectors.loginPage.passwordInputField)
         .should('have.attr', 'placeholder', l10n.en.loginPage.form.passwordInputField)
         .and('have.value', '')
         .and('have.attr', 'type', 'password')
@@ -23,10 +26,37 @@ describe('LoginPage: Given Login page is opened', { testIsolation: false }, () =
     });
 
     it('LoginPage: Then user see Login button', () => {
-      cy.get(loginPage.loginButton)
+      cy.get(selectors.loginPage.loginButton)
         .should('have.value', l10n.en.loginPage.form.loginButton)
         .and('be.visible')
         .and('be.enabled');
     });
   });
+
+  context('LoginPage: When user logs in with valid credentials', () => {
+    before(() => {
+      cy.visit('/');
+      cy.login(users.standard_user.username, users.standard_user.password);
+    });
+
+    it('Login Page: Then user should be redirected to the Inventory page', () => {
+      cy.url().should('eq', pages.inventoryPage);
+    });
+
+    it('Login Page: Then user can see inventry list on the Inventory page ', () => {
+      cy.get(selectors.inventoryPage.inventory_list).should('be.visible');
+    });
+  });
+
+  context.skip('LoginPage: When user logs in with empty username', () => {});
+
+  context.skip('LoginPage: When user logs in with empty password', () => {});
+
+  context.skip('LoginPage: When user logs in with invalid username', () => {});
+
+  context.skip('LoginPage: When user logs in with invalid password', () => {});
+
+  context.skip('LoginPage: When user logs in with locked_out_user credentials', () => {});
+
+  context.skip('LoginPage: When user logs in with problem_user credentials', () => {});
 });
--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,6 +1,15 @@
-export const loginPage = {
+const loginPage = {
   userNameInputField: '[data-test="username"]',
   passwordInputField: '[data-test="password"]',
   loginButton: '[data-test="login-button"]',
   errorMessage: '[data-test="error"]',
+};
+
+const inventoryPage = {
+  inventory_list: '[data-test="inventory-list"]',
+};
+
+export default {
+  loginPage,
+  inventoryPage,
 };
