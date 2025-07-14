a7923745d3ac1f9bc84ea192158fcd18d7c97eb3
vlad <vladsheverda7@gmail.com>
2025-07-14 20:00:24 +0400

    refactor selector and test files for inventory page

--- cypress/integration/ui/inventory-page.ui.spec.js
+++ cypress/integration/ui/inventory-page.ui.spec.js
@@ -4,64 +4,62 @@ describe('InventoryPage: Given Inventory page is opened', { testIsolation: false
       cy.visit('/');
       cy.login(users.standard_user.username, users.standard_user.password);
     });
-    it('InventoryPage: Then user can see page title', () => {
+    it('InventoryPage: Then the user can see page title', () => {
       cy.get(headerComponent.title).should('be.visible').and('have.text', l10n.header.title);
     });
 
-    it('InventoryPage: Then user can see product label', () => {
+    it('InventoryPage: Then the user can see product label', () => {
       cy.get(headerComponent.secondaryHeader.productLabel)
         .should('be.visible')
         .and('have.text', l10n.header.secondaryHeader.title);
     });
 
-    it('InventoryPage: Then user can see sorting button', () => {
+    it('InventoryPage: Then the user can see sorting button', () => {
       cy.get(headerComponent.secondaryHeader.sortContainer.defaultSorting).should(
         'have.text',
         l10n.header.secondaryHeader.sortContainer
       );
     });
 
-    it('InventoryPage: Then user can see product names', () => {
+    it('InventoryPage: Then the user can see product names', () => {
       const expectedNames = products.map((product) => product.name);
       inventoryPage.getNameList().should('deep.equal', expectedNames);
     });
 
-    it('InventoryPage: Then user can see product description', () => {
+    it('InventoryPage: Then the user can see product description', () => {
       const expectedDescriptions = products.map((product) => product.description);
       inventoryPage.getDescriptionList().should('deep.equal', expectedDescriptions);
     });
 
-    it('InventoryPage: Then user can see product prices', () => {
+    it('InventoryPage: Then the user can see product prices', () => {
       const expectedPrices = products.map((product) => product.price);
       inventoryPage.getPriceList().should('deep.equal', expectedPrices);
     });
+
+    it('InventoryPage: Then the user can not see cart icon bage', () => {
+      cy.get(headerComponent.shoppingCartIconBadge).should('not.exist');
+    });
   });
 
-  context('InventoryPage: When user clicks add to cart button', () => {
-    before(() => {
+  context('InventoryPage: When the user adds/removes backpack to cart', () => {
+    beforeEach(() => {
       cy.visit('/');
       cy.login(users.standard_user.username, users.standard_user.password);
       cy.get(inventoryPage.addToCart('backpack')).click();
     });
+
     it('InventoryPage: Then shopping cart icon badge icon equals 1', () => {
-      cy.get(headerComponent.shoppingCartIcon.shoppingCartBadge).should('have.text', 1);
+      cy.get(headerComponent.shoppingCartIconBadge).should('have.text', 1);
       cy.get(inventoryPage.removeFromCart('backpack')).click();
     });
-  });
 
-  context('InventoryPage: When user clicks remove button', () => {
-    before(() => {
-      cy.visit('/');
-      cy.login(users.standard_user.username, users.standard_user.password);
-      cy.get(inventoryPage.addToCart('backpack')).click();
-      cy.get(inventoryPage.removeFromCart('backpack')).click();
-    });
     it('InventoryPage: Then shopping cart icon is not displayed', () => {
-      cy.get(headerComponent.shoppingCartIcon.shoppingCartBadge).should('not.exist');
+      cy.get(inventoryPage.removeFromCart('backpack')).click();
+      cy.get(headerComponent.shoppingCartIconBadge).should('not.exist');
     });
   });
 
-  context('InventoryPage: When the user change sorting by name', () => {
+  context('InventoryPage: When the user changes sorting', () => {
     before(() => {
       cy.visit('/');
       cy.login(users.standard_user.username, users.standard_user.password);
@@ -76,13 +74,6 @@ describe('InventoryPage: Given Inventory page is opened', { testIsolation: false
       cy.get(headerComponent.secondaryHeader.sortContainer.productSortContainer).select('az');
       const expectedNameList = products.map((product) => product.name);
       inventoryPage.getNameList().should('deep.equal', expectedNameList.sort());
-    });
-  });
-
-  context('InventoryPage: When the user change sorting by price', () => {
-    before(() => {
-      cy.visit('/');
-      cy.login(users.standard_user.username, users.standard_user.password);
     });
 
     it('InventoryPage: Product list is sorted in Low to High order', () => {
--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -15,19 +15,22 @@ const inventoryPage = {
   addToCart: (productKey) => `[data-test="add-to-cart-sauce-labs-${productKey}"]`,
   removeFromCart: (productKey) => `[data-test="remove-sauce-labs-${productKey}"]`,
   getNameList() {
-    return cy
-      .get(this.inventoryItem.name)
-      .then(($els) => [...$els].map((el) => el.innerText.trim()));
+    return cy.get(this.inventoryItem.name).then(($els) => {
+      if (!$els.length) return [];
+      return [...$els].map((el) => el.innerText.trim());
+    });
   },
   getDescriptionList() {
-    return cy
-      .get(this.inventoryItem.description)
-      .then(($els) => [...$els].map((el) => el.innerText.trim()));
+    return cy.get(this.inventoryItem.description).then(($els) => {
+      if (!$els.length) return [];
+      return [...$els].map((el) => el.innerText.trim());
+    });
   },
   getPriceList() {
-    return cy
-      .get(this.inventoryItem.price)
-      .then(($els) => [...$els].map((el) => el.innerText.slice(1).trim()));
+    return cy.get(this.inventoryItem.price).then(($els) => {
+      if (!$els.length) return [];
+      return [...$els].map((el) => el.innerText.slice(1).trim());
+    });
   },
 };
 
@@ -37,20 +40,13 @@ const headerComponent = {
     burgerMenu: '.bm-burger-button button',
     logout: '[data-test="logout-sidebar-link"]',
   },
-  shoppingCartIcon: {
-    shoppingCartBadge: '[data-test="shopping-cart-badge"]',
-  },
+  shoppingCartIconBadge: '[data-test="shopping-cart-badge"]',
+
   secondaryHeader: {
     productLabel: '[data-test="title"]',
     sortContainer: {
       productSortContainer: '[data-test="product-sort-container"]',
       defaultSorting: '[data-test="active-option"]',
-      sortOptions: {
-        az: '.product_sort_container option[value="az"]',
-        za: '.product_sort_container option[value="za"]',
-        lowToHigh: '.product_sort_container option[value="lohi"]',
-        highToLow: '.product_sort_container option[value="hilo"]',
-      },
     },
   },
 };
